85bb00454f759f75be79bbfa9af7c1c20d87860c
Crumbed <kaiharrelson5466@gmail.com>
2024-05-01 10:12:17 -0400

    testing time? frfr 2

---
+++ run.bat
@@ -0,0 +1,9 @@
+SET CANVAS_API_KEY = $(cat .env)
+
+SET CONTACT_LOG_ID = "" # spreadsheet id
+SET SYN_INFO_ID = "" # spreadsheet id
+
+SET AUTO_MSG_PATH = "" # path to auto message response
+SET POLICY_PATH = "" # path to makeup policy file
+
+lehman_auto.exe
